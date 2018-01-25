9593edf2316eef6d30060d5c46b10f4b97d64658
Charlie Smurthwaite <charlie@atechmedia.com>
2018-01-25 15:17:23 +0000

    initial commit

---
+++ uuid.c
@@ -0,0 +1,33 @@
+#include <openssl/md5.h>
+#include <uuid/uuid.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  // Ensure we have enough arguments
+  if(argc < 3) return(1);
+
+  // Fetch the arguments and their length
+  char * unique_id = argv[1];
+  char * accountcode = argv[2];
+  int unique_id_length = strlen(unique_id);
+  int accountcode_length = strlen(accountcode);
+
+  // Allocate a string and copy the 2 arguments into it
+  int string_length = unique_id_length+accountcode_length+1;
+  char * string = malloc(string_length);
+  memcpy(string, unique_id, unique_id_length);
+  string[unique_id_length] = '-';
+  memcpy(string + unique_id_length + 1, accountcode, accountcode_length);
+
+  // Allocate memory for the MD5 hash and its UUID string
+  unsigned char hash[16];
+  unsigned char uuid[37];
+  MD5(string, string_length, hash);
+  uuid_unparse(hash, uuid);
+  uuid[36] = '\n';
+  write(1,uuid,37);
+  return(0);
+}
